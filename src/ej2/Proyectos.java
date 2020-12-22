4224cf0b87880d4bed079c094e2cba29af1c5f9b
Brais <brais.cotelo@udc.es>
2020-12-22 20:48:14 +0100

    Ejercicio 2 casi acabado

--- src/ej2/Proyectos.java
+++ src/ej2/Proyectos.java
@@ -35,23 +35,23 @@ public class Proyectos extends Componentes{
         return new String(info);
     }
 
-    public static void main(String[] args) {
-        Proyectos p1 = new Proyectos("DS");
-        Equipo e1 = new Equipo("A");
-        Trabajador t1 = new Trabajador("Ger", 20);
-        t1.setHours(20);
-        e1.add(t1);
-        p1.add(e1);
-        System.out.println(p1.getInfo());
-        System.out.println("\n");
-
-        Proyectos p2 = new Proyectos("SO");
-        Equipo e2 = new Equipo("B");
-        Trabajador t2 = new Trabajador("Brr", 30);
-        t2.setHours(25);
-        e2.add(t2);
-        p2.add(e2);
-        System.out.println(p2.getInfo());
-
-    }
+//    public static void main(String[] args) {
+//        Proyectos p1 = new Proyectos("DS");
+//        Equipo e1 = new Equipo("A");
+//        Trabajador t1 = new Trabajador("Ger", 20);
+//        t1.setHours(20);
+//        e1.add(t1);
+//        p1.add(e1);
+//        System.out.println(p1.getInfo());
+//        System.out.println("\n");
+//
+//        Proyectos p2 = new Proyectos("SO");
+//        Equipo e2 = new Equipo("B");
+//        Trabajador t2 = new Trabajador("Brr", 30);
+//        t2.setHours(25);
+//        e2.add(t2);
+//        p2.add(e2);
+//        System.out.println(p2.getInfo());
+//
+//    }
 }
